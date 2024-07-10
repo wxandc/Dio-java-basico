e71aa4158e0c75b978af26d73a602aec5d0d1380
wxandc <wxandc@gmail.com>
2024-07-10 08:28:08 -0300

    up aula 03 de variaveis

--- java-anatomia-class/src/ConteudoOrganizado/Aula03/TiposDeVariaveis.java
+++ java-anatomia-class/src/ConteudoOrganizado/Aula03/TiposDeVariaveis.java
@@ -1,4 +1,23 @@
 package ConteudoOrganizado.Aula03;
 public class TiposDeVariaveis {
-    
+    //Para numeros inteiros usamos:
+    //byte - E representação para variaveis com -128 de capacidade;
+    //Logo;
+    byte numerosExp1 = 127;
+    //short - E representação para variaveis com -32768 de capacidade;
+    //Logo;
+    short numerosExp2 = 32767;
+    //int - E representação para variaveis com -2147483648 de capacidade;
+    //Logo;
+    int numerosExp3 = 2147483647;
+    //long - E representação para variaveis com -9223372036854775808 de capacidade + o sufixo (L);
+    //Logo;
+    long numerosExp4 = 9223372036854775807L;
+    //Para numeros fracionados usamos:
+    //float - E representação para variaveis com -3,4028E+38 de capacidade;
+    //Logo;
+    float numerosExp5 = 3.14F;
+    //double - E representação para variaveis com -1,7976E+308 de capacidade;
+    //Logo;
+    double numerosExp6 = 2.500;
 }
