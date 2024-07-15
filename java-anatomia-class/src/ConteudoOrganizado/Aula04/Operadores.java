c6b965c042f160f2833dd1525e4cdb16f79ac184
wxandc <wxandc@gmail.com>
2024-07-15 08:06:21 -0300

    up operadores

---
+++ java-anatomia-class/src/ConteudoOrganizado/Aula04/Operadores.java
@@ -0,0 +1,27 @@
+package ConteudoOrganizado.Aula04;
+public class Operadores {
+    // Operadores de Atribuição:
+
+    //1. String nome = "William";
+    //2. int idade = 35;
+    //3. double peso = 81.5;
+    //4. char = 'M';
+    //5. boolean doadorOrgao = false;
+    //6. dataNascimento = new Date();
+
+    // Operadores aritimeticos:
+
+    //1. double soma = 10.5 + 15.7;
+    //2. int subtração = 113 - 25;
+    //3. int multiplicação = 20 * 7;
+    //4. int divisão = 15 / 3;
+    //5. int modulo = 18 % 3;
+    //6. double resusltado = (10 * 3) + (20 / 4) - 1;
+
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+        
+        System.out.println(nomeCompleto);
+
+    }
+}
---
+++ java-anatomia-class/src/ConteudoOrganizado/Exercicios/OperadoresExerc.java
@@ -0,0 +1,26 @@
+package ConteudoOrganizado.Exercicios;
+public class OperadoresExerc {
+    public static void main(String[] args) {
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    }
+}
